6d988e28598c0c698011b6c588fe97682459abba
黄新民 <huangxinmin@hzwanglan.com>
2025-02-12 10:56:18 +0800

    feat:添加git支持和代码规范

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // 新功能
+        'fix',  // 修复问题
+        'docs', // 文档修改
+        'style', // 代码格式（不影响功能，如空格、分号等）
+        'refactor', // 代码重构
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'perf' // 性能优化
+      ]
+    ],
+    'subject-case': [2, 'always', 'sentence-case']
+  }
+};
